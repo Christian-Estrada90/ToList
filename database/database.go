b4e9a0c700f340b5e50bb32bca8ba10bde6394b5
Danii :) <charlialvarezbrau@hotmail.com>
2024-08-19 11:01:11 -0500

    Cambios para el consumo en el Frontend

--- database/database.go
+++ database/database.go
@@ -8,7 +8,7 @@ import (
 
 func DbConnection() (*sql.DB, error) {
 	// Cambia 'root' si usas otro usuario, y '12345' es tu contraseña actual
-	connectionString := "root:root@tcp(127.0.0.1:3306)/crud_golang?charset=utf8&parseTime=True&loc=Local"
+	connectionString := "root:12345@tcp(127.0.0.1:3306)/crud_golang?charset=utf8&parseTime=True&loc=Local"
 
 	db, err := sql.Open("mysql", connectionString)
 	if err != nil {
--- main.go
+++ main.go
@@ -10,6 +10,22 @@ import (
 	"github.com/gorilla/mux"
 )
 
+func enableCors(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
+		// Si es una solicitud OPTIONS, solo respondemos con el encabezado de CORS y devolvemos
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
+
 func main() {
 	router := mux.NewRouter()
 	router.HandleFunc("/users", userService.CreateUser).Methods(http.MethodPost)
@@ -21,6 +37,8 @@ func main() {
 	router.HandleFunc("/actividades", userService.CreateActividad).Methods(http.MethodPost)
 	router.HandleFunc("/actividades", userService.GetActividad).Methods(http.MethodGet)
 
+	corsRouter := enableCors(router)
+
 	fmt.Print("Listenning on 5000")
-	log.Fatal(http.ListenAndServe(":5000", router))
+	log.Fatal(http.ListenAndServe(":5000", corsRouter))
 }
